2cbebaa9f12b99293444bd044a1f68dad80ecaa2
vyshnavi892 <vasam.vyshnavi@dxc.com>
2025-04-15 10:40:55 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -1,25 +1,38 @@
 provider "google" {
   project     = var.project
   region      = var.region
-  credentials = "keys.json"
+  credentials = var.credentials
+}
+
+resource "google_compute_network" "my-vpc" {
+  name = var.my-vpc
+  
+}
+
+resource "google_compute_subnetwork" "subnetwork" {
+  name = var.subnetwork
+  ip_cidr_range = var.subnet_cidr
+  network = google_compute_network.my-vpc.id
+  region = "us-central1"
+  
 }
 
 resource "google_compute_instance" "my-instance" {
-  name         = "my-instance"
-  machine_type = "e2-medium"
+  name         = var.instance_type
+  machine_type = var.machine_type
   zone         = var.zone
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"  # Changed the direct value here for demonstration
+      image = var.os_image 
     }
   }
 
   network_interface {
-    network = "default"
+    network = google_compute_network.my-vpc.id
+    subnetwork = google_compute_subnetwork.subnetwork.id
 
     access_config {
-      # This will assign an ephemeral external IP address
     }
   }
 }
