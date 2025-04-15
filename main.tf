f3b52b51dd8f7721569ca876715987540b187ef8
vyshnavi892 <vasam.vyshnavi@dxc.com>
2025-04-15 19:17:12 +0530

    Add files via upload

--- main.tf
+++ main.tf
@@ -4,6 +4,10 @@ provider "google" {
   credentials = var.credentials
 }
 
+locals {
+  setup_name = "dxc"
+}
+
 resource "google_compute_network" "my-vpc" {
   name = var.my-vpc
   
@@ -14,13 +18,15 @@ resource "google_compute_subnetwork" "subnetwork" {
   ip_cidr_range = var.subnet_cidr
   network = google_compute_network.my-vpc.id
   region = "us-central1"
-  
 }
 
 resource "google_compute_instance" "my-instance" {
   name         = var.instance_type
   machine_type = var.machine_type
   zone         = var.zone
+
+  tags = [local.setup_name]
+    
 
   boot_disk {
     initialize_params {
